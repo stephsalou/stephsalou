3ae8f68e283eb2b1489382fcdd01c679272bffaf
stephane <stephane.gambo@smile.ci>
2021-04-26 11:03:06 +0000

    --- Auto-Git Commit ---

--- pages/index.vue
+++ pages/index.vue
@@ -77,7 +77,7 @@ export default class Index extends Vue {
     font-size: 2rem;
     &::selection {
       color: red;
-      background-color: yellow;
+      background-color:url("../assets/image/");
     }
     &:hover {
       font-size: 3.5rem;
