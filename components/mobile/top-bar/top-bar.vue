6fb8a5e211d6527cb86722e9d1ce0c5221eb6087
stephane <stephane.gambo@smile.ci>
2021-04-26 10:23:19 +0000

    --- Auto-Git Commit ---

--- components/mobile/top-bar/top-bar.vue
+++ components/mobile/top-bar/top-bar.vue
@@ -1,7 +1,7 @@
 <template src="./top-bar.html"></template>
 <script lang="ts">
 import 'reflect-metadata'
-import { Component, Prop, Vue } from 'vue-property-decorator'
+
 
 @Component({})
 export default class TopBar extends Vue {
