a238d787c06c5498c5c036739b8f95345ac0aeec
stephane <stephane.gambo@smile.ci>
2021-04-26 10:44:22 +0000

    --- Auto-Git Commit ---

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@ export default class Index extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss">
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
@@ -72,6 +72,8 @@ export default class Index extends Vue {
   padding-top: 15px;
 }
 .nameLetter {
-  font-size: 50px;
+  &:hover {
+    font-size: 50px;
+  }
 }
 </style>
